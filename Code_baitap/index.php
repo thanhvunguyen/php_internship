69289f033a855fa8eded8fea17c1effa09952573
tnkttruong <tnkttruong@gmail.com>
2016-12-14 10:01:10 +0700

    demo

---
+++ Code_baitap/index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<h1>Hello I'm Tnkt Truong</h1>
+<p>
+<?php
+   echo "Hi there.\n";
+   $answer = 6 * 7;
+   echo "The answer is $answer, what was the question again?\n";
+?>
+</p>
+<p>Yes another paragraph.</p>`
+</body>
+</html>
