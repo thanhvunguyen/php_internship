93a7b7868c84830334b4858322b4bb699c21a92e
Your Name <you@example.com>
2016-12-19 21:19:32 +0700

    update code

---
+++ bai1-2/fields.php
@@ -0,0 +1,20 @@
+
+<html>
+<body>
+
+<form>
+  First game:<br>
+  <input type="text" name="firstgame">
+  <br>
+  Second game:<br>
+  <input type="text" name="secondgame">
+  <br>
+	<p> Game favourite:
+</p>
+  <input type="radio" name="game" value="male" checked> lol<br>
+  <input type="radio" name="game" value="female"> Dota 2<br>
+  <input type="radio" name="game" value="other"> Other 
+</form>
+
+</body>
+</html>
---
+++ bai1-2/table.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
+</head>
+<body>
+
+<table style="width:100%">
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th> 
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Tran Anh</td>
+    <td>Thang</td>
+    <td>22</td>
+  </tr>
+  <tr>
+    <td>Ho Viet</td>
+    <td>Nghia</td>
+    <td>22</td>
+  </tr>
+  <tr>
+    <td>Duong</td>
+    <td>Qua</td>
+    <td>56</td>
+  </tr>
+  <tr>
+    <td>Tieu Long</td>
+    <td>Nu</td>
+    <td>76</td>
+  </tr>
+</table>
+
+</body>
+</html>
